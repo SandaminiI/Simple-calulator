fb0baa371bda137867097b37a32e7df3943144bd
Sandamini Indrachapa <130564612+SandaminiI@users.noreply.github.com>
2024-05-21 16:09:04 +0530

    Implemented calculator functions

--- app/src/main/java/com/example/tutorial04/MainActivity.kt
+++ app/src/main/java/com/example/tutorial04/MainActivity.kt
@@ -2,10 +2,35 @@ package com.example.tutorial04
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.EditText
+import android.widget.Toast
+import com.example.tutorial04.models.Calculator
 
 class MainActivity : AppCompatActivity() {
+
+    lateinit var edtNumber1:EditText
+    lateinit var edtNumber2:EditText
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        edtNumber1 = findViewById(R.id.edtNumber1)
+        edtNumber2 = findViewById(R.id.edtNumber2)
+    }
+    fun buttonClick(v:View){
+        var ans = 0.0
+        val calculator = Calculator(
+            edtNumber1.text.toString().toDouble(),
+            edtNumber2.text.toString().toDouble())
+        when(v.id){
+            R.id.btnPlus -> ans = calculator.add()
+            R.id.btnMinus -> ans = calculator.subtract()
+            R.id.btnMultiply -> ans = calculator.multiply()
+            R.id.btnDivide -> ans = calculator.divide()
+        }
+        Toast.makeText(this,"Answer =$ans",Toast.LENGTH_LONG).show()
+        //this will print the output on the terminal
     }
 }
